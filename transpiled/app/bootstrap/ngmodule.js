baf28cdd386ba7f0294dddeb05a2c0a13a48f444
christopherthielen <github@sandgnat.com>
2016-07-09 20:30:19 -0500

    chore(*): committing transpiled files

--- transpiled/app/bootstrap/ngmodule.js
+++ transpiled/app/bootstrap/ngmodule.js
@@ -8,9 +8,9 @@ var angular = require("angular");
 var angular_ui_router_1 = require("angular-ui-router");
 require("d3");
 var ui_router_visualizer_1 = require("ui-router-visualizer");
-// Create the angular module "demo".
+// Create the angular 1 module "demo".
 //
 // Since it is exported, other parts of the application (in other files) can then import it and register things.
 // In bootstrap.js, the module is imported, and the components, services, and states are registered.
-exports.ngmodule = angular.module("demo", [angular_ui_router_1.default, ui_router_visualizer_1.default]);
+exports.ngmodule = angular.module("demo", [angular_ui_router_1.default, ui_router_visualizer_1.default, 'ui.router.upgrade']);
 //# sourceMappingURL=ngmodule.js.map
